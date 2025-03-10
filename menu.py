f84791805ee4bf49df953549a24eb9e3f4e2d669
Lucast34 <lucast34t@gmail.com>
2025-03-10 18:21:32 -0300

    Menu_python

---
+++ menu.py
@@ -0,0 +1,15 @@
+# aqui será o menu de seleção de cada atividade
+
+#import {menu das pastas}
+
+def menu():
+
+    while True:
+
+        opcao = str(input("place holder:"))
+
+        print (opcao)
+
+        break
+
+menu()
