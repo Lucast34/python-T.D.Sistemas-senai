cb80d242643d11ab9426e74442dcd218fd6875a4
Lucast34 <lucast34t@gmail.com>
2025-05-23 20:20:27 -0300

    atv05

---
+++ PY/atv05/atv05.py
@@ -0,0 +1,16 @@
+# variavel
+i=1
+
+# input
+
+print("*"*4,"Programa de tabuada","*"*4)
+
+num = int(input("Digite um numero: "))
+
+print("*"*4,"Tabuada do ",num,"*"*4)
+
+# loop 
+
+while i <= 10:
+    print(f'{num}x{i}={num*i}')
+    i= i+1
