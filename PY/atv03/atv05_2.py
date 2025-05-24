0e179848fcaa0d2ea0e10d27f08b7e2339693608
Lucast34 <lucast34t@gmail.com>
2025-05-23 21:04:55 -0300

    atv03

---
+++ PY/atv03/atv05_2.py
@@ -0,0 +1,12 @@
+# estrutura 
+
+for i in range(1,101,1):
+
+    print("Multiplo de dez >>> ", i)if i % 10 == 0 else print(i)
+    
+    """
+    if i % 10 == 0:
+        print("Multiplo de dez >>> ", i)
+    else:
+        print(i)
+    """
