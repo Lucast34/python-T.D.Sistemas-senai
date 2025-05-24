d44b1d3a0ac92830ac1475bd9993a09d5fd5a608
Lucast34 <lucast34t@gmail.com>
2025-05-24 15:08:00 -0300

    atv04 lista

---
+++ PY/atv04/atv04.py
@@ -0,0 +1,39 @@
+# Lista
+lista_num=[5,8,2,9,1]
+
+# Crescente
+
+lista_num.sort()
+
+
+print(lista_num)
+
+# Decrescente
+
+lista_num.reverse()
+
+print(lista_num)
+
+# Adicionando na lista
+
+lista_num.append(7)
+
+lista_num.insert(2,3)
+
+print(lista_num)
+
+lista_num.remove(9)
+
+lista_num.insert(1,10)
+
+print(lista_num)
+
+# Excluindo
+
+lista_num.pop(2)
+
+lista_num.pop(3)
+
+lista_num.pop(4)
+
+print(lista_num)
