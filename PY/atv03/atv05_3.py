87144964e2199315358288c9a7b2c88abfe5f3d1
Lucast34 <lucast34t@gmail.com>
2025-05-23 21:18:45 -0300

    atv05 refatorado

--- PY/atv03/atv05_3.py
+++ PY/atv03/atv05_3.py
@@ -1,24 +1,11 @@
-# input
-num1 = int(input("Digite um numero >> "))
-num2 = int(input("Digite outro numero >> "))
-num3 = int(input("Digite mais um numero >> "))
+#variavel 
 
-# sim eu sei que é um jeito idiota
-if num1 > num2 and num1>num3:
-    print(f'O maior numero: {num1}')
-    if num2 > num3:
-        print(f'Segundo maior: {num2}\nMenor: {num3}')
-    else:
-        print(f'Segundo maior: {num3}\nMenor: {num2}')
-elif num2 > num1 and num2 > num3:
-    print(f'O maior numero: {num2}')
-    if num1 > num3:
-        print(f'Segundo maior: {num1}\nMenor: {num3}')
-    else:
-        print(f'Segundo maior: {num3}\nMenor: {num1}')
-else:
-    print(f'O maior numero: {num3}')
-    if num1 > num2:
-        print(f'Segundo maior: {num1}\nMenor: {num2}')
-    else:
-        print(f'Segundo maior: {num2}\nMenor: {num1}')
+num_maior = 0
+
+# loop 
+
+for i in range(0,3):
+    num = int(input("Digite um numero: "))
+    if num_maior < num: num_maior = num
+
+print("Numero maior: ",num_maior)
