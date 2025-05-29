15240048d9975357a6332d75985b0f87f558e0de
Lucast34 <lucast34t@gmail.com>
2025-05-29 18:05:39 -0300

    salvando

--- PY/POO/Poo.py
+++ PY/POO/Poo.py
@@ -1,8 +1,13 @@
+# classe
 class Exemplo:
+    # atributo
     x = int
 
+# Atribuindo uma variavel a uma classe
 p1 = Exemplo()
 
+# classe possui uma variavel declarada como inteiro, com isso pegamos o atributo como 5
 p1.x = 5
 
+# printando uma soma simples o resultado esperado é 10
 print(p1.x+5)
