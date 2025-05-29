f8a36298c29f0bf1002e4c315b320e8ef2ffda4a
Lucast34 <lucast34t@gmail.com>
2025-05-29 19:07:41 -0300

    classes, objetos e atributos com/sem instancia

--- PY/POO/POO_tudo/class.py
+++ PY/POO/POO_tudo/class.py
@@ -10,10 +10,24 @@ class Cachorro:
         self.nome = nome # <- gera uma copia desse atributo, quando ele for modificado somento ele vai ser afetado
         self.idade = idade 
 
+    """
+    def __init__(self,nome,idade,cor):
+        self.nome = nome
+        self.idade = idade
+        self.cor = cor
+    
+    diferentes constructors não é permitido pelo pyhton (ainda),
+    Porém existe um outra maneira de contornar esse problema. 
+    """
+
+
+# A classe tem que ser declarado em uma variavek
 doguinho = Cachorro("menino",14)
+doguinho_2 =Cachorro("Luke",11)
 
 
+print(f'{doguinho.especie}\n{doguinho.nome}\n{doguinho.idade}')
 
-print(doguinho.nome)
+print(f'{doguinho_2.nome}\n{doguinho_2.idade}\nprint passou')
 
 
