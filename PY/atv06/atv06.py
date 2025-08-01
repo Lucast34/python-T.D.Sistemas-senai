1421c0f8a7101d5739ac73387e961020d7f51c87
Lucast34 <lucast34t@gmail.com>
2025-08-01 15:59:37 -0300

    Update

--- PY/atv06/atv06.py
+++ PY/atv06/atv06.py
@@ -5,37 +5,43 @@ Deve incluir:{
     "nome": "nome"<-String,
     "estoque" : "estoque"<-int,
     "preco" : "preco"<- float lol
-
-
 }
 
+
+1 - Cadastro de Produto Você precisa criar um programa que armazene informações de um produto em um dicionário.
+As informações devem incluir nome, preço e quantidade em estoque. 
+Depois, o programa deve exibir todas as informações do produto.
 """
 
-
-
-produto_dic={
-    "nome" : None,
-    "estoque" : 0,
-    "preco": 0.0
+produto_dic = {
+    "nome": [],
+    "estoque": [],
+    "preco": []
 }
 
+print("1 - cadastro\n2 - sair")
+opcao = int(input(">>> "))
 
-while True:
 
-    decisao = int(input("Escolha >> "))
+match opcao:
+    case 1:
+        print("*"*4,"Cadastro","*"*4)
+        nome_dic = str(input("Nome >> "))
+        
+        estoque_dic = int(input("Estoque >> ")) 
+        
+        preco_dic = float(input("Estoque >> "))
 
-    match decisao:
-        case 1:
-            print("placeholder add")
-            break
-        case 2:
-            print("placeholder update")
-            break
-        case _:
-            print("invalide input")
+        produto_dic["nome"].append(nome_dic)
 
-    decisao = int(input("Escolha pra continuar >>"))
+        produto_dic["estoque"].append(estoque_dic)
 
-    if(decisao == 1):print("passou")
-    else: break
+        produto_dic["preco"].append(preco_dic)
 
+        print(produto_dic)
+
+        for i in produto_dic:
+            print(f'Nome do produto: {produto_dic["nome"][i]}\nEstoque: {produto_dic["estoque"][i]}\nPreco: {produto_dic["preco"][i]}')
+    case 2:
+        print("Programa encerrado") 
+         
